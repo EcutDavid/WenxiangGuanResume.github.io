cd30c7d05c17a227778c395f3bfd5a58c0817c79
EcutDavid <Ecutwxguan@163.com>
2015-05-30 19:11:48 +0800

    Just try io

---
+++ Script/Main.js
@@ -0,0 +1,12 @@
+﻿angular.module("TODO", [])
+    .controller("controller", function ($scope) {
+        $scope.name = "Guan";
+        $scope.todoList = [{ name: "备案", description: "提交身份证照片，"},
+        { name: "Pro Angular JS", description: "两章，阅读加练习"},
+        { name: "Cracking the Coding Interview", description: "3小时的Coding练习。。" },
+        { name: "Check 51JOB", description: "检查。。。"}];
+
+        $scope.submit = function () {
+            $scope.todoList.push({ name: $scope.nameInputText, description: $scope.descriptionInputText });
+        }
+    });
